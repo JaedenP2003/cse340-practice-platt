44bbe451280101bca8478f29ab57b2b49e01fedf
Jaeden Platt <jaeden.platt2003@gmail.com>
2026-05-23 14:17:39 -0600

    Finished Part 1

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -1,31 +1,37 @@
 import { Router } from 'express';
-
-// Create a new router instance
-const router = Router();
-
 // TODO: Add import statements for controllers and middleware
 import { addDemoHeaders } from '../middleware/demo/headers.js';
 import { catalogPage, courseDetailPage } from './catalog/catalog.js';
 import { homePage, aboutPage, demoPage, testErrorPage } from './index.js';
 import { facultyListPage, facultyDetailPage } from './faculty/faculty.js';
 
-// TODO: Add route definitions
+// Create a new router instance
+const router = Router();
 
+// Add catalog-specific styles to all catalog routes
+router.use('/catalog', (req, res, next) => {
+    res.addStyle('<link rel="stylesheet" href="/css/catalog.css">');
+    next();
+});
+
+// Add faculty-specific styles to all faculty routes
+router.use('/faculty', (req, res, next) => {
+    res.addStyle('<link rel="stylesheet" href="/css/faculty.css">');
+    next();
+});
+
+// TODO: Add route definitions
 // Home and basic pages
 router.get('/', homePage);
 router.get('/about', aboutPage);
-
 // Course catalog routes
 router.get('/catalog', catalogPage);
-router.get('/catalog/:courseId', courseDetailPage);
-
+router.get('/catalog/:slugId', courseDetailPage);
 // Demo page with special middleware
 router.get('/demo', addDemoHeaders, demoPage);
-
 // Faculty routes
 router.get('/faculty', facultyListPage);
-router.get('/faculty/:facultyId', facultyDetailPage);
-
+router.get('/faculty/:facultySlug', facultyDetailPage);
 // Route to trigger a test error
 router.get('/test-error', testErrorPage);
 
